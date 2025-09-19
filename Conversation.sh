6b16b8deb0ca23e5bf9b65273b0b68fc9aef59b6
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-19 09:41:59 +0800

    conv b/w ramesh & suresh

---
+++ Conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh:Hi Suresh, How are you?"
+echo "Suresh:Hello Ramesh , i am fine .How are you?"
+echo "Ramesh:What are you learning recently?"
+echi "Suresh:I am learning shell scripting Ramesh"
