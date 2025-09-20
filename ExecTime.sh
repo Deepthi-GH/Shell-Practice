4495705a50d6c063bd41e52d57b13b5b6e514fb6
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-20 23:37:09 +0800

    exec time

--- ExecTime.sh
+++ ExecTime.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-date =$(date)
-echo "today date is:$date" 
-start_time=$(DATE+%s)
+x =$(date)
+echo "today date is:$x" 
+start_time=$(DATE +%s)
 
 SLEEP 10
-end_time=$(DATE+%s)
+end_time=$(DATE +%s)
 total_time=$(($start_time-$end_time))
 echo "total time taken to execute a script  is:$total_time
