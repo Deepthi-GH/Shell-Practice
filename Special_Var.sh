a479a4f4e7100b3a6c1ed04aeed4c691f3452d99
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-21 16:48:31 +0800

    spe vars

--- Special_Var.sh
+++ Special_Var.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "all the variables passed to the script:$@"
 echo "all the variables passed to the script:$*"
+echo "shell script name:$0"
+echo "current working directory:$pwd"
+echo "who is running the script:$USER"
+echo "homwe directory of user:$HOME"
