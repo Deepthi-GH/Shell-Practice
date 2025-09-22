336ecc241f0e883d2f37507a8c6546e0f2a26887
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-22 22:00:43 +0800

    compare a no with 10

--- Conditions.sh
+++ Conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "pelase enter a number"
  read NUMBER
- if[ $NUMBER -lt 10]; then
+ if[ $NUMBER -lt 10] then
     echo "given number is less than 10"
  else
     echo "given number is greater than 10"
