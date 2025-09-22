97b0f0f7f562813322a62fbad127f61cf5d90f4e
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-22 23:39:37 +0800

    exit status

--- Exit_Status.sh
+++ Exit_Status.sh
@@ -2,12 +2,14 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ] 
 then
-    echo "error::please run this script with root privilige"
+    echo "error::please run this script with root priviliges"
+    exit 1
 fi
 dnf install mysql -y
 if [ $? -ne 0 ] 
 then
     echo "error::mysql installation failed"
+    exit 1
 else
     echo "mysql installation is success"
 fi    
