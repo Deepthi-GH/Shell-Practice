a01883a5bf08d1e59c84235065d51a776f902b66
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-23 23:18:22 +0800

    tee command

--- Create_Logs.sh
+++ Create_Logs.sh
@@ -9,7 +9,7 @@ LOGS_FOLDER=/var/log/shell-script
 SCRIPT_NAME=$( echo $0|cut -d "." -f1 )
 LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
 mkdir -p $LOGS_FOLDER
-echo "script started at: $(date)"
+echo "script started at: $(date)" |tee -a $LOG_FILE
 if [ $USERID -ne 0 ] 
 then
     echo "error::please run this script with root priviliges"
