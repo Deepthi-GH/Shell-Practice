374422dd03219f468e678dc353218bc6056bcda9
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-09-25 16:38:33 +0800

    testing

--- file1.sh
+++ file1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
- error(){
-    echo "there is an error in $LINENO and command is $BASH_COMMAND"
- }
+# error(){
+ #   echo "there is an error in $LINENO and command is $BASH_COMMAND"
+# }
 
-trap error err
+trap 'echo "there is an error in $LINENO and command is $BASH_COMMAND"' err
  echo "hello world"
  afdgfdhgdhf #This is an error.shell sends ERR signal.
  echo "hi"
