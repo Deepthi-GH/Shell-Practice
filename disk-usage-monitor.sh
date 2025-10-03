3cf6cda6816d51d536484d6704a1087a9b04644e
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-10-03 11:13:09 +0800

    disk usage

--- disk-usage-monitor.sh
+++ disk-usage-monitor.sh
@@ -8,12 +8,12 @@ DISK_THRESHOLD=2 #in projects we keep it as 75
  while IFS= read -r line
  do 
         USED=$($line |awk '{ print $6 }'|cut -d% -f1)
-        Mount_PATH=$($line|awk '{ print $7 }')
+        #MOUNT_PATH=$($line|awk '{ print $7 }')
    
-    if [ $USED -ge  $DISK_THRESHOLD ]
+    if [ $USED -ge $DISK_THRESHOLD ]
     then 
        
-        echo  -e "High usage on below paths: \n $Mount_PATH $USED"
+        echo  -e "High usage on below paths: \n $USED"
       
    fi
 
